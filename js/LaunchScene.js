07c79b782348475d69f1f407e842cf99fcb77564
ZombieWarlock <cameronspiva129@gmail.com>
2022-03-04 19:13:52 -0500

    made various changes to plus and minus

--- js/LaunchScene.js
+++ js/LaunchScene.js
@@ -41,7 +41,7 @@ class LaunchScene extends Phaser.Scene{
 
 
         //Adds background for stats buttons
-        obj.backRect = this.add.rectangle(GAME_WIDTH/2,GAME_HEIGHT/2,GAME_WIDTH/1.4,GAME_HEIGHT/6,0xffffff);
+        obj.backRect = this.add.rectangle(GAME_WIDTH/2,GAME_HEIGHT/2,GAME_WIDTH/1.4,GAME_HEIGHT/2,0x23d5f1);
 
         //First stat's rectangle x coord
         let first_stat_x = obj.backRect.x - obj.backRect.x/1.8;
@@ -52,7 +52,7 @@ class LaunchScene extends Phaser.Scene{
         //THIS IS MEANT TO BE CHANGED LATER
         //whether that be the colors, this is only a basic layout
         for (let i = 1; i <= Object.keys(this.stats.resources).length; i++) {
-            obj.tool_rects.push(this.add.rectangle(first_stat_x,obj.backRect.y,obj.backRect.width/5.3,obj.backRect.height/1.3,0xd93093));
+            obj.tool_rects.push(this.add.rectangle(first_stat_x,obj.backRect.y+(obj.backRect.height/3.5),obj.backRect.width/5.3,obj.backRect.height/3,0xffffff));
             
             first_stat_x+= DIST_BTWN_STATS;
         }
@@ -69,22 +69,26 @@ class LaunchScene extends Phaser.Scene{
                 if(this.stats.resources[key]>=MAX_RESOURCE){return;}//Stops if it's at MAX_RESOURCE
                 if(sum(Object.values(this.stats.resources))+1>MAX_SUM_RESOURCE){return;}//Stops if the sum of all resources is at MAX_SUM_RESOURCE
                 this.stats.resources[key]++;
-                stats_buttons[key].label.setText(`${key}:${this.stats.resources[key]}`)
+                stats_buttons[key].resourcelabel.setText(key)
+                stats_buttons[key].resourcecount.setText(this.stats.resources[key])
             };
 
             let minus = ()=>{
-                if(this.stats.resources[key]<=0){return;}
+                if(this.stats.resources[key]<=1){return;}
                 this.stats.resources[key]--;
-                stats_buttons[key].label.setText(`${key}:${this.stats.resources[key]}`)
+                stats_buttons[key].resourcelabel.setText(key)
+                stats_buttons[key].resourcecount.setText(this.stats.resources[key])
+
             };
 
             
             //Makes the label, plus and minus buttons 
             stats_buttons[key] = {
 
-                label:this.add.text(first_stat_x,obj.backRect.y-obj.backRect.y/11,`${key}:${this.stats.resources[key]}`,).setOrigin(.5,.5),
-                plus:createButton(first_stat_x,obj.backRect.y-5,'+',this,plus,undefined,'20px Comic Sans'),
-                minus:createButton(first_stat_x,obj.backRect.y+20,'-',this,minus,undefined,'20px Comic Sans'),
+                resourcelabel:this.add.text(first_stat_x,obj.backRect.y-obj.backRect.height/2+20,key,{font:'20px',color:'#487D9E'}).setOrigin(.5,.5),
+                resourcecount:this.add.text(first_stat_x,obj.tool_rects[0].y-25,this.stats.resources[key],{font:'25px',color:'#487D9E'}).setOrigin(.5,.5),
+                plus:createButton(first_stat_x,obj.tool_rects[0].y+3,'ADD',this,plus,null,'20px Comic Sans'),
+                minus:createButton(first_stat_x,obj.tool_rects[0].y+28,'REMOVE',this,minus,null,'20px Comic Sans'),
 
             }
 
